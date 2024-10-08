aa878c1188afbc04fa66e1d0066264de3143c839
MuriloMMarangoni <3msxxvi@gmail.com>
2024-10-08 19:55:49 -0300

    Signed-off-by: MuriloMMarangoni <3msxxvi@gmail.com>

--- socketa.py
+++ socketa.py
@@ -1,43 +1,79 @@
-import socket # módulo de comunicação de processos ou de máquinas da mesma rede
-host = socket.gethostname() # nome do host ou ip, gethostname usa o localhost
-ip = socket.gethostbyname(host) # diz o ip de um dispositivo
-port = 12345 # porta de acesso 1-65000
-obj = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # cria um socket com comunicação(ipv4,tcp)
-obj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # deixa o servidor elegivel a ser iniciado 1s depois do encerramento
-def ask() -> bool:
-    perm = input("Press 0 for server or 1 for client\n")
+import socket
+import threading # incluir depois
+import pickle # não sei o que é mas da pra aprender
+
+def tcplh():
+    d = {
+        's' : socket.socket(socket.AF_INET,socket.SOCK_STREAM),
+        'c' : socket.socket(socket.AF_INET,socket.SOCK_STREAM),
+        'ip' : socket.gethostbyname(socket.gethostname()),
+        'localhost' : socket.gethostname(),
+        'port' : 12345
+    }
+    return d
+def tcpl():
+    d = {
+        's' : socket.socket(socket.AF_INET,socket.SOCK_STREAM),
+        'c' : socket.socket(socket.AF_INET,socket.SOCK_STREAM),
+        'ip_server' : '0.0.0.0',
+        'ip' : '',
+        'port' : 12345
+    }
+    return d
+def udp():
+    raise NotImplementedError
+
+def client(d:dict):
+    c = d['c']
+    ip = d['ip']
+    port = d['port']
+    if mostrar_ip:
+        ip = input('Insira o ip do servidor:\n')
+    c.connect((ip,port))
+    print(c.recv(1024).decode())
+    c.send("O cliente está funcionando".encode())
+    c.close()
+def server(d:dict):
+    s = d['s']
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # deixa o servidor elegivel a ser iniciado 1s depois do encerramento
     try:
-        perm = bool(int((perm)))
+        ip = d['ip_server']
     except Exception:
-        print('! Invalid input')
-    else:
-        return perm
-    
-def server(host,port,obj):
-    obj.bind((host,port)) # associa o host e a porta (servidores)
-    obj.listen() # aceita conexões (servidores)
-    host_client,port_client = obj.accept() # quem conectou[socket,[ip,porta]] (servidores)
-    print(f"[Server] Connection stablished with {port_client[0]} with port {port_client[1]}")
+        ip = d['ip']
+    port = d['port']
+    s.bind((ip,port))
+    if mostrar_ip:
+        print(f"o ip pra acesso remoto é {socket.gethostbyname(socket.gethostname())}")
+    s.listen(1)
+    c,adr = s.accept()
+    c.send(f"O servidor está funcionando!".encode())
+    print(c.recv(1024).decode())
     while True:
-        data = host_client.recv(1024) # recebe os dados do cliente
-        if data == b'': # se não receber encerra
-            break
-        print(f"[Client] {data.decode()}")
-        i = input("[Server] ")
-        host_client.sendall(i.encode()) # mostra pro cliente
-    obj.close()
-
-def client(host,port,obj):
-    i = ' '
-    obj.connect((host,port))
-    while i != '':
-        i = input("[Client] ")
-        obj.sendall(i.encode())
-        data = obj.recv(1024) # para a execução e espera uma resposta
-        print(f"[Server] {data.decode()}")
-    obj.close()
-
-if ask():
-    client(ip,port,obj)
-else:
-    server(ip,port,obj)
+        break
+    c.close()
+if __name__ == '__main__':
+    print(f"{30*'-'}\nEscolha qual o tipo de comunicação Cliente-Servidor")
+    tipo = input("1-TCP Localhost\n2-TCP Local\n3-UDP\n")
+    if tipo not in '123':
+        raise SystemExit
+    print(f"{30*'-'}\nEscolha qual o seu papel na comunicação")
+    papel = input("1-Servidor\n2-Cliente\n")
+    if papel not in '123':
+        raise SystemExit
+    mostrar_ip = False #mostrar o ip pra conexão remota (tcpl)
+    if tipo == '1':
+        if papel == '1':
+            server(tcplh())
+        if papel == '2':
+            client(tcplh())
+    elif tipo =='2':
+        mostrar_ip = True
+        if papel == '1':
+            server(tcpl())
+        if papel == '2':
+            client(tcpl())
+    else:
+        if papel == '1':
+            server(udp())
+        if papel == '2':
+            client(udp())
