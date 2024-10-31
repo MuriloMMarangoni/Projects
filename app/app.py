8e3b66429eb106a28744f53c8c021688c89b1134
MuriloMMarangoni <3msxxvi@gmail.com>
2024-10-31 14:35:18 -0300

    Signed-off-by: MuriloMMarangoni <3msxxvi@gmail.com>

--- app/app.py
+++ app/app.py
@@ -6,12 +6,18 @@
 # trazer o pdf já pronto pro app
 # fazer um app com tela de cadastro
 # quando der enter, ativar o botão
+# bugs: senhas em * não correspondem pro login
+#
+# sugestões: colocar indicadores nos Entrys
+# colocar indicador de tela de login,registro e principal
+#
 import tkinter as tk
 import tkinter.font as tkFont
 import sqlite3
 
 menu = tk.Tk()
 menu.geometry('1000x500')
+menu.title("Login")
 ver_senha_confirmacao = tk.BooleanVar()
 fonte_padrao = tkFont.Font(family='Arial',size=12)
 fonte_hover = tkFont.Font(family='Arial',size=12,underline=1)
@@ -41,6 +47,7 @@ def validar():
     if login.get() == 'admin':
         if senha.get() == 'admin':
             tela_principal.tkraise()
+            menu.title("Utilitários")
         else:
             if len(tela_de_login.winfo_children()) == 6: # se na tela tiver 2 entry,caixa,botao,registre  e mensagem de erro
                 temp.destroy()
@@ -77,26 +84,50 @@ def mostrar_widgets():
     validar()
     print(tela_de_login.winfo_children())
 
+def registre():
+    '''
+    Muda pra tela de registrar
+    '''
+    tela_de_registro.tkraise()
+    menu.title("Registre-se")
+def logue():
+    '''
+    Muda pra tela de login
+    '''
+    tela_de_login.tkraise()
+    menu.title('Login')
+
 ver_senha = tk.Checkbutton(tela_de_login,text='Visualizar Senha',variable=ver_senha_confirmacao,command=lambda:cripto() if ver_senha_confirmacao.get() else discripto())
 confirmar = tk.Button(tela_de_login,text='Próximo',command=validar)
 registrar = tk.Label(tela_de_login,text='Não tem uma conta?\nRegistre-se.',font=fonte_padrao)
-registrar.bind('<Button-1>',lambda event:tela_de_registro.tkraise())
+registrar.bind('<Button-1>',lambda event:registre())
 registrar.bind('<Enter>', lambda event:registrar.config(font=fonte_hover))
 registrar.bind('<Leave>', lambda event:registrar.config(font=fonte_padrao))
 
 tela1 = [login,senha,ver_senha,confirmar,registrar]
 for each in tela1: each.pack()
 
-l1 = tk.Label(tela_principal,text='bem-vindo')
+l1 = tk.Label(tela_principal,text='App de utilidades')
 voltar = tk.Label(tela_principal,text='<-')
-voltar.bind('<Button-1>',lambda event:tela_de_login.tkraise())
+voltar.bind('<Button-1>',lambda event:logue())
 
 tela2 = [l1,voltar]
 for each in tela2: each.pack()
 
-l2 = tk.Label(tela_de_registro,text='registre')
+nome           = tk.Entry(tela_de_registro) 
+registro       = tk.Entry(tela_de_registro) 
+senha_registro = tk.Entry(tela_de_registro)
+senha_registro_confirmar = tk.Entry(tela_de_registro)
+def verificar():
+    if senha_registro.get() == senha_registro_confirmar.get():
+        tela_principal.tkraise()
+    else:
+        tk.Label(tela_de_registro,text='As senhas são diferentes',bg='red').pack()
+confirmar_registro = tk.Button(tela_de_registro,text='Verificar',command=verificar)
+l3 = tk.Label(tela_de_registro,text='<-')
+l3.bind('<Button-1>',lambda event: logue())
 
-tela3 = [l2]
+tela3 = [nome,registro,senha_registro,senha_registro_confirmar,confirmar_registro,l3]
 for each in tela3: each.pack()
 
 tela_de_login.tkraise()
