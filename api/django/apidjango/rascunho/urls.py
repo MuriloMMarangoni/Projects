60eb3c9bb3802f6d007c1c5e33041caa1f0a032f
maxinne <3msxxvi@gmail.com>
2025-06-18 08:17:17 -0300

    Signed-off-by: maxinne <3msxxvi@gmail.com>

--- api/django/apidjango/rascunho/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path # ligar um url a um html
-from . import views # arquivo com as funções de html
-
-urlpatterns = [
-    path('',views.home), # referencia uma função de views.py pra uma url http://127.0.0.1:8000/rascunho/
-    path('texto/', views.texto), #http://127.0.0.1:8000/rascunho/texto/
-    path('outrapg/',views.outrapg),
-    path('outrapgp',views.outrapgp)
-]
--- bank/django/bank/django_bank/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-import datetime
-# Create your views here.
-context = {
-    'data': datetime.date.today()
-}
-def home(request):
-    return render(request,'home.html',context)
