831aae584d29f835496708754b379ffa553bbe2a
maxinne <3msxxvi@gmail.com>
2025-06-30 14:43:30 -0300

    Signed-off-by: maxinne <3msxxvi@gmail.com>

---
+++ Teoria/c#.cs
@@ -0,0 +1,7 @@
+/*
+Pra usar o c# instale o dotnet sdk
+abra um terminal em CTRL+SHIFT+'
+use no terminal
+           dotnet new console -n MeuProjeto
+vai criar uma pasta do projeto, abra o vscode dentro dessa pasta, apartir dai execute os programas c#
+*/
---
+++ Teoria/c#/Program.cs
@@ -0,0 +1,8 @@
+﻿bool a = true; // bool
+a = a && false; // operação AND
+int i = 1;
+uint ii = 2;
+char d = 'd';
+string palavra = "palavra";
+Console.WriteLine(a); // mostra saída
+Console.WriteLine(palavra);
